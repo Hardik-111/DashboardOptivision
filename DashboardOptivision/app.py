7ca56e2325b3afe069e6e9dcb7e42389327efc91
HARDIK-111 <ihardik.112@gmail.com>
2025-04-15 23:39:23 +0530

    Toogle effect

--- DashboardOptivision/app.py
+++ DashboardOptivision/app.py
@@ -72,4 +72,4 @@ def serve_static(filename):
     return send_from_directory('.', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5100)
---
+++ DashboardOptivision/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./templates/**/*.html",
+      "./static/**/*.js"
+    ],
+    darkMode: 'class', // <== enable class-based dark mode
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
